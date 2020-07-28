fc2edfbcac0cd031c84e4eeeec5ccaf91d1b51f1
Ryan Stimmler <rstimmler4@gmail.com>
2020-07-28 13:37:27 -0400

    fix: untrusted implementation

--- src/App.js
+++ src/App.js
@@ -1,115 +1,69 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
+import { Authenticate } from "@workpathco/client";
+import cookie from "js-cookie";
 import "./App.css";
-import { getAuthUrl } from "./auth-helpers";
-function logout(e) {
-  e.preventDefault();
-  const logoutUrl = new URL(`${process.env.REACT_APP_AUTH_URL}/auth/logout`);
-  const params = new URLSearchParams({
-    redirect_to: `${process.env.REACT_APP_APP_URL}/`
-  });
-  logoutUrl.search = params.toString();
-  window.location.href = "";
-  sessionStorage.removeItem("token");
-  window.location.assign(logoutUrl.toString());
+const TOKEN_KEY = "_wp_token";
+const authentication = new Authenticate({
+  redirect_uri: `${window.location.protocol}//${window.location.host}`,
+  client_id: process.env.REACT_APP_CLIENT_ID,
+  auth_domain: process.env.REACT_APP_API_URL,
+  id_domain: process.env.REACT_APP_ID_URL,
+});
+async function getUser(token) {
+  return fetch(`${process.env.REACT_APP_ID_URL}/auth/me`, {
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer: ${token}`,
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+  }).then((response) => response.json());
+}
+function logout() {
+  cookie.remove(TOKEN_KEY);
+  authentication.logout();
 }
 function App() {
-  const [url, setUrl] = useState(false);
-  const [params, setParams] = useState(new URLSearchParams(""));
   const [user, setUser] = useState({});
-  const token = sessionStorage.getItem("token");
   useEffect(() => {
-    getUrl();
-  }, []);
-  async function getUrl() {
-    if (!token) {
-      const url = await getAuthUrl();
-      setUrl(url);
-    }
-  }
-  useEffect(() => {
-    if (params.get("code")) {
-      getBearer(params);
-    }
-  }, [params]);
-  async function getBearer(_params) {
     try {
-      const response = await fetch(
-        `${process.env.REACT_APP_AUTH_URL}/auth/code`,
-        {
-          method: "POST", // *GET, POST, PUT, DELETE, etc.
-          mode: "cors", // no-cors, *cors, same-origin
-          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "same-origin", // include, *same-origin, omit
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
-          redirect: "follow", // manual, *follow, error
-          referrer: "no-referrer", // no-referrer, *client
-          body: _params.toString() // body data type must match "Content-Type" header
-        }
-      );
-      const data = await response.json();
-      const state = _params.get("state");
-      window.location.href = "/";
-      sessionStorage.removeItem(`login-code-verifier-${state}`);
-      if (data) {
-        sessionStorage.setItem("token", data.token);
-        const response = await fetch(`${process.env.REACT_APP_AUTH_URL}/me`, {
-          headers: {
-            Authorization: `Bearer ${data.token}`
-          }
+      const searchParams = new URLSearchParams(window.location.search);
+      if (cookie.get(TOKEN_KEY)) {
+        const token = JSON.parse(cookie.get(TOKEN_KEY));
+        getUser(token.access_token).then((user) => setUser(user));
+      } else if (searchParams.get("state")) {
+        authentication.consume().then(() => {
+          const token = authentication.memory.getToken();
+          cookie.set(TOKEN_KEY, JSON.stringify(token));
+          getUser(token.access_token).then((user) => setUser(user));
+          window.history.replaceState(
+            {},
+            document.title,
+            window.location.href.replace(window.location.search, "")
+          );
         });
-        const user = await response.json();
-        setUser(user);
       }
-    } catch (err) {
-      /* noop */
-    }
-  }
-  useEffect(() => {
-    if (token) {
-      getUser();
-    }
-  }, [token]);
-  async function getUser() {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_AUTH_URL}/me`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-      const user = await response.json();
-      setUser(user);
-    } catch (err) {}
-  }
-  useEffect(() => {
-    if (window.location.search) {
-      const _params = new URLSearchParams(window.location.search);
-      const state = _params.get("state");
-      const code_verifier = sessionStorage.getItem(
-        `login-code-verifier-${state}`
-      );
-      if (code_verifier) {
-        const requestParams = new URLSearchParams({
-          code: _params.get("code"),
-          code_method_challenge: "s256",
-          grant_type: "authorization_code",
-          redirect_uri: `${window.location.protocol}//${window.location.host}/`,
-          scope: "offline_access",
-          client_id: process.env.REACT_APP_CLIENT_ID,
-          code_verifier
-        });
-        setParams(requestParams);
-      }
+    } catch (e) {
+      // noop
     }
   }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {url && (
-          <a className="App-link" href={url}>
+        {!user.person && (
+          <a
+            className="App-link"
+            href=""
+            onClick={() => {
+              authentication.login();
+              return false;
+            }}
+          >
             Login
           </a>
         )}
